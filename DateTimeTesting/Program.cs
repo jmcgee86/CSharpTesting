b30540027da75c089a0140e916e34a912b1e4205
Jim McGee <jmcgee86@gmail.com>
2018-07-19 15:12:23 -0400

    initial commit

---
+++ DateTimeTesting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testing
+{
+    class HolidayChecker
+    {
+
+        public static List <DateTime> holidays = new List <DateTime>{new DateTime (2018,12,31), new DateTime(2018, 11, 25)};
+
+        public static void AddHoliday(DateTime date)
+        {
+            holidays.Add(date);
+        }
+        public static bool CheckTodayHoliday(DateTime date)
+        {
+            var checkHolidays = holidays.Contains(date);
+            if (checkHolidays)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public 
+        static void Main(string[] args)
+        {
+            AddHoliday(DateTime.Today);
+            Console.WriteLine(CheckTodayHoliday(DateTime.Today));            
+        }
+    }
+}
